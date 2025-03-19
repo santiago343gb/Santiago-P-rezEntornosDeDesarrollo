3619a6c1238a1fc1c95f4e400ab590e6c97bd119
santiago <santiago.perez2@isepceu.es>
2025-03-19 19:59:17 +0100

    Guardando cambios antes de cambiar de rama

---
+++ SantiagoPerez_Rectangulo.java
@@ -0,0 +1,62 @@
+public class SantiagoPerez_Rectangulo {
+
+    
+    private double ladoA;
+    private double ladoB;
+    private String color;
+
+    
+    public SantiagoPerez_Rectangulo() {
+        
+        this.ladoA = 0.0;
+        this.ladoB = 0.0;
+        this.color = "Desconocido";
+    }
+
+   
+    public SantiagoPerez_Rectangulo(double ladoA, double ladoB, String color) {
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
+        this.color = color;
+    }
+
+  
+
+    public double getLadoA() {
+        return ladoA;
+    }
+
+    public void setLadoA(double ladoA) {
+        this.ladoA = ladoA;
+    }
+
+    public double getLadoB() {
+        return ladoB;
+    }
+
+    public void setLadoB(double ladoB) {
+        this.ladoB = ladoB;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    
+    @Override
+    public String toString() {
+        return "Rectángulo [Lado A = " + ladoA + ", Lado B = " + ladoB + ", Color = " + color + "]";
+    }
+
+    public static void main(String[] args) {
+
+        SantiagoPerez_Rectangulo rectangulo = new SantiagoPerez_Rectangulo(5.0, 3.0, "Rojo");
+        
+
+        System.out.println(rectangulo.toString());
+    }
+}
