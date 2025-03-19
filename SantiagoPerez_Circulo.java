442d2df7dcd1b233d26e2edce989252c9eafd3f8
santiago <santiago.perez2@isepceu.es>
2025-03-19 19:44:44 +0100

    desarrollo

---
+++ SantiagoPerez_Circulo.java
@@ -0,0 +1,41 @@
+public class SantiagoPerez_Circulo {
+
+    
+    private double radio;
+    private String color;
+
+   
+    public void InicialesAlumno_Circulo() {
+        this.radio = 0.0;
+        this.color = "sin color";
+    }
+
+   
+    public void InicialesAlumno_Circulo(double radio, String color) {
+        this.radio = radio;
+        this.color = color;
+    }
+
+    
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+   
+    @Override
+    public String toString() {
+        return "Circulo [radio=" + radio + ", color=" + color + "]";
+    }
+}
